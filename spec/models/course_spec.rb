e3d0e9348e25ff55d5da41b50e8d74acda8f6324
Rob Wise <robert.wise@outlook.com>
2014-08-20 01:33:12 -0400

    refactor course_spec.rb

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,17 +5,14 @@ RSpec.describe Course, :type => :model do
   it { should respond_to(:level) }
   it { should respond_to(:title) }
   it { should respond_to(:description) }
-  context "with acceptable attributes" do
-      let!(:course) { FactoryGirl.build(:course) }
-      subject { course }
-
-      it { should be_valid }
-    end
 
   describe "validation" do
     let!(:course) { FactoryGirl.build(:course) }
     subject { course }
 
+    context "with acceptable attributes" do
+      it { should be_valid }
+    end
     it { should validate_presence_of(:department) }
     it { should validate_presence_of(:level) }
     it { should validate_numericality_of(:level).is_greater_than(0) }
