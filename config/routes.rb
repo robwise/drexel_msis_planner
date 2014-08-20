1e105d33cad7c711596fac8bb3e80e18bd16b9a7
Rob Wise <robert.wise@outlook.com>
2014-08-20 02:32:56 -0400

    add courses index and partial views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+  resources :users, :courses
 end
---
+++ spec/features/courses/course_index_spec.rb
@@ -0,0 +1,5 @@
+feature 'Course index page' do
+  scenario 'viewing the course index page' do
+    before { visit courses_path }
+  end
+end
