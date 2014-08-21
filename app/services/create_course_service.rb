6a2803151d39120d0a69af5e3fe64bafcbff6582
Rob Wise <robert.wise@outlook.com>
2014-08-20 23:39:33 -0400

    add degree_requirement to create_course_service.rb

--- app/services/create_course_service.rb
+++ app/services/create_course_service.rb
@@ -3,6 +3,7 @@ class CreateCourseService
     course = course.find_or_create_by!(department: 'INFO', level: '530') do |course|
       course.title = 'Foundations of Information Systems'
       course.description = 'Some descriptive text about the course.'
+      course.degree_requirement = :required_course
       course.confirm!
     end
   end
