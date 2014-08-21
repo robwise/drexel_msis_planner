b7a7379ea409f8eb5997d33ef58bd6f532feb64e
Rob Wise <robert.wise@outlook.com>
2014-08-21 00:00:56 -0400

    change course factory to facilitate multiple instantiations

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,11 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+# include ActiveSupport
 
 FactoryGirl.define do
   factory :course do
     department "INFO"
-    level 530
-    title "Foundations of Information Systems"
-    description "Some text describing the course."
+    sequence(:level) { |n| n + 529 }
+    title Faker::Lorem.words(4).to_s.titleize
+    description Faker::Lorem.paragraph
     degree_requirement "free_elective"
   end
 end
