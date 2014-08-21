ae57fa18df3849b1ccec5303a0f015d0ac2f230e
Rob Wise <robert.wise@outlook.com>
2014-08-20 23:42:53 -0400

    fix errors in CreateCourseService.rb

--- app/services/create_course_service.rb
+++ app/services/create_course_service.rb
@@ -1,10 +1,9 @@
 class CreateCourseService
   def call
-    course = course.find_or_create_by!(department: 'INFO', level: '530') do |course|
+    course = Course.find_or_create_by!(department: 'INFO', level: '530') do |course|
       course.title = 'Foundations of Information Systems'
       course.description = 'Some descriptive text about the course.'
       course.degree_requirement = :required_course
-      course.confirm!
     end
   end
 end
