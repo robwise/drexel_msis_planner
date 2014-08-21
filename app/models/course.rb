d91d6fbaaff174908a9711e4d64fd1d5e1673ef8
Rob Wise <robert.wise@outlook.com>
2014-08-20 23:58:36 -0400

    add full_id method to course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,8 @@ class Course < ActiveRecord::Base
   validates :level, presence: true,
                     numericality: { greater_than: 0, less_than: 2000 }
   validates :degree_requirement, presence: true
+
+  def full_id
+    "#{department} #{level}"
+  end
 end
