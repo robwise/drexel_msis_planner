cec66377b2bec827733b593946939374310e2b57
Rob Wise <robert.wise@outlook.com>
2014-08-26 01:56:12 -0400

    fix formatting issue in enrolled_in.rb

--- app/models/enrolled_in.rb
+++ app/models/enrolled_in.rb
@@ -10,28 +10,30 @@ class EnrolledIn < ActiveRecord::Base
   belongs_to :course
   validate :is_unique_validator
 
-  def is_unique_validator
-    already_exists = EnrolledIn.find_by(user_id: user_id,
-                                        course_id: course_id)
-    if already_exists
-      errors.add(:course_id, 'already exists for this user')
-    end
-  end
-
   def self.already_enrolled?(user, course)
     EnrolledIn.find_by(user_id: user.id, course_id: course.id).nil? ? false : true
   end
 
-  # Quarter codes are YYYY-QQ, where QQ is 15 (Fall), 25 (Winter), 35 (Spring)
-  # and 45 (Summer)
-  def quarter_code_validator
-    year = quarter / 100
-    season = (quarter - (year * 100))
-    bad_year = (year > (Time.now.year + 1)) || year < 1980
-    bad_season = season % 15 > 0 || !season.between?(15, 45)
-    bad_length = quarter.to_s.length != 6
-    if bad_year || bad_season || bad_length || quarter.nil? || quarter.blank?
-      errors.add(:quarter, 'is not a valid quarter code')
+  private
+    def is_unique_validator
+      already_exists = EnrolledIn.find_by(user_id: user_id,
+                                          course_id: course_id)
+      if already_exists
+        errors.add(:course_id, 'already exists for this user')
+      end
     end
-  end
+
+
+    # Quarter codes are YYYY-QQ, where QQ is 15 (Fall), 25 (Winter), 35 (Spring)
+    # and 45 (Summer)
+    def quarter_code_validator
+      year = quarter / 100
+      season = (quarter - (year * 100))
+      bad_year = (year > (Time.now.year + 1)) || year < 1980
+      bad_season = season % 15 > 0 || !season.between?(15, 45)
+      bad_length = quarter.to_s.length != 6
+      if bad_year || bad_season || bad_length || quarter.nil? || quarter.blank?
+        errors.add(:quarter, 'is not a valid quarter code')
+      end
+    end
 end
