d440653f6a89ec26bcf81d7b5c5a9ccea3af6812
Rob Wise <robert.wise@outlook.com>
2014-08-26 23:18:38 -0400

    rename model, model spec, and factory

--- app/models/taken_course.rb
+++ app/models/taken_course.rb
@@ -1,4 +1,4 @@
-class EnrolledIn < ActiveRecord::Base
+class TakenCourse < ActiveRecord::Base
   enum grade: ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D',
                'D-', 'F', 'Withdrew', 'Incomplete', 'In Progress', 'Registered']
   validates :user_id, presence: true
@@ -10,14 +10,14 @@ class EnrolledIn < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
 
-  def self.already_enrolled?(user, course)
-    EnrolledIn.find_by(user_id: user.id, course_id: course.id).nil? ? false : true
+  def self.already_taken?(user, course)
+    TakenCourse.find_by(user_id: user.id, course_id: course.id).nil? ? false : true
   end
 
   private
 
     def is_unique_validator
-      already_exists = EnrolledIn.find_by(user_id: user_id,
+      already_exists = TakenCourse.find_by(user_id: user_id,
                                           course_id: course_id)
       if already_exists
         errors.add(:course_id, 'already exists for this user')
