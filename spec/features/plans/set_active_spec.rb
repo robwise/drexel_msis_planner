47db30cd254031a669883648cb75606a2fdd261a
Rob Wise <robert.wise@outlook.com>
2014-09-08 13:03:56 -0400

    add additional tests for checking if a plan has a 'set active' button

--- spec/features/plans/set_active_spec.rb
+++ spec/features/plans/set_active_spec.rb
@@ -3,17 +3,20 @@ feature "Set a Plan as Active" do
   let!(:plan1) { create :plan, user: user }
   let!(:plan2) { create :plan, user: user }
 
-  # FIX: replace database calls with viewing changes on the page
   scenario "as a normal user" do
     signin_user(user)
     visit user_plans_path(user)
     expect(page).to have_content(plan1.name)
     expect(page).to have_content(plan2.name)
     expect(is_active?(plan1)).to eq false
+    expect(have_set_active_button_for(plan1)).to eq true
     expect(is_active?(plan2)).to eq true
+    expect(have_set_active_button_for(plan2)).to eq false
     click_on 'set active'
-    expect(is_active?(plan2)).to eq false
     expect(is_active?(plan1)).to eq true
+    expect(have_set_active_button_for(plan1)).to eq false
+    expect(is_active?(plan2)).to eq false
+    expect(have_set_active_button_for(plan2)).to eq true
   end
 
   private
@@ -25,6 +28,12 @@ feature "Set a Plan as Active" do
     def active_status_for(plan)
       page.within("##{plan.name}") do
         find('.active-status').text
+      end
+    end
+
+    def have_set_active_button_for(plan)
+      page.within("##{plan.name}") do
+        not has_no_button?('set active')
       end
     end
 
