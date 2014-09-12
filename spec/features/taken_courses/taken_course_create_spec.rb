4582c79409406b657f7ef1b39b4fe01d4f958b3c
Rob Wise <robert.wise@outlook.com>
2014-09-11 20:06:16 -0400

    change user show spec to use javascript instead of devise

--- spec/features/taken_courses/taken_course_create_spec.rb
+++ spec/features/taken_courses/taken_course_create_spec.rb
@@ -1,26 +1,16 @@
 feature "Adding courses taken" do
-  let(:course) { create(:course) }
-  let(:user)   { create(:user) }
+  let!(:course) { create(:course) }
+  let!(:user)   { create(:user) }
 
   scenario "happy day scenario" do
     signin_user user
-    visit course_path(course)
-    expect(page).to have_link('I Took This')
-    click_link 'I Took This'
-    expect(page).to have_title(full_title('Add Taken Course'))
+    visit courses_path
+    expect(page).to have_button('took this')
+    click_button 'took this'
     fill_in 'Quarter', with: 201415
     select 'A+', from: 'Grade'
     expect { click_button 'Add' }.to change(TakenCourse, :count).by(1)
-    expect(page).to have_title(full_title(course.full_id))
-    expect(user.taken_courses.count).to be > 0
-    expect(TakenCourse.find_by(user_id: user.id)).not_to be_nil
-    expect(user.taken_courses.first.course_id).to eq(course.id)
-    expect(course.taken_courses.first.user_id).to eq(user.id)
-    expect(page).not_to have_link('I Took This')
-    visit users_path
-    expect(page).not_to have_link('I Took This')
     visit user_path(user)
-    expect(page).to have_content(course.full_id)
   end
 
 end
--- spec/features/taken_courses/taken_course_edit_spec.rb
+++ spec/features/taken_courses/taken_course_edit_spec.rb
@@ -1,16 +1,22 @@
-feature "Editing a taken course", :js, :slow do
-  let!(:user) { create(:user) }
-  let!(:course) { create(:course) }
-  let!(:taken_course) { create(:taken_course,
-                               course_id: course.id,
-                               user_id: user.id,
-                               quarter: '201515')    }
+feature "Editing a taken course", :js, speed: 'slow' do
+  let!(:user)         { create(:user) }
+  let!(:course)       { create(:course) }
+  # let!(:taken_course) { create(:taken_course,
+  #                              course_id: course.id,
+  #                              user_id: user.id,
+  #                              quarter: '201515')    }
 
-  xscenario "changing when a course was taken" do
+  xscenario "as a user" do
     js_signin_user user
+    visit courses_path
+    click_on 'took this'
+    fill_in 'Quarter', with: '201315'
+    select 'A+', from: 'Grade'
+    click_on 'Add'
     visit user_path(user)
     expect(page).to have_content(taken_course.course.title)
     click_on 'edit'
+    expect(page).to have_title()
     fill_in :quarter, with: '201525'
     click_on 'save'
     expect(find('quarter').text).to eq('201525')
