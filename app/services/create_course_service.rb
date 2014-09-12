ebf120b207cdce1eba3e12ab45a2e8b4f85ab07e
Rob Wise <robert.wise@outlook.com>
2014-09-12 15:47:17 -0400

    convert to double quotes

--- app/services/create_course_service.rb
+++ app/services/create_course_service.rb
@@ -1,13 +1,13 @@
 class CreateCourseService
   def call
-    Course.find_or_create_by!(department: 'INFO', level: '530') do |course|
-      course.title = 'Foundations of Information Systems'
-      course.description = 'Some descriptive text about the course.'
+    Course.find_or_create_by!(department: "INFO", level: "530") do |course|
+      course.title = "Foundations of Information Systems"
+      course.description = "Some descriptive text about the course."
       course.degree_requirement = :required_course
     end
-    Course.find_or_create_by!(department: 'INFO', level: '532') do |course|
-      course.title = 'Software Development'
-      course.description = 'Some descriptive text about the course.'
+    Course.find_or_create_by!(department: "INFO", level: "532") do |course|
+      course.title = "Software Development"
+      course.description = "Some descriptive text about the course."
       course.degree_requirement = :required_course
     end
   end
