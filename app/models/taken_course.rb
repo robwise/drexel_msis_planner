6a21c868f8459e64f279b1479f57cb70cd7b98dc
Rob Wise <robert.wise@outlook.com>
2014-09-12 23:56:14 -0400

    refactor TakenCourse#quarter_code_validator

--- app/models/taken_course.rb
+++ app/models/taken_course.rb
@@ -4,7 +4,7 @@ class TakenCourse < ActiveRecord::Base
   validates :user_id, presence: true
   validates :course_id, presence: true
   validates :grade, presence: true
-  validates :quarter, presence: true#, quarter: true
+  validates :quarter, presence: true
   validate :quarter_code_validator, unless: Proc.new { quarter.nil? }
   validate :is_unique_validator, on: :create
   belongs_to :user
@@ -17,27 +17,17 @@ class TakenCourse < ActiveRecord::Base
   private
 
     def quarter_code_validator
-      if quarter.nil?
-        errors.add(:quarter, "cannot be nil")
-        return false
-      end
       quarter_object = Quarter.new(quarter)
-      if quarter_object.valid?
-        if quarter_object.to_date < Time.now
-          return true
-        else
-          errors.add(:quarter, "cannot be in the future.")
-          return false
-        end
-      else
+      if (not quarter_object.valid?)
         errors.add(:quarter, "is not a valid quarter code")
-        return false
+      elsif quarter_object.to_date > Time.now
+        errors.add(:quarter, "cannot be in the future.")
       end
     end
 
     def is_unique_validator
       already_exists = TakenCourse.find_by(user_id: user_id,
-                                          course_id: course_id)
+                                           course_id: course_id)
       if already_exists
         errors.add(:course_id, 'already exists for this user')
       end
