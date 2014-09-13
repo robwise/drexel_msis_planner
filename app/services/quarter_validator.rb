fd2e7b319ac7b5a8bf8729dc01297304694973df
Rob Wise <robert.wise@outlook.com>
2014-09-12 20:42:19 -0400

    refactor quarter_validator

--- app/services/quarter_validator.rb
+++ app/services/quarter_validator.rb
@@ -1,21 +1,25 @@
 class QuarterValidator < ActiveModel::EachValidator
+  @@valid_seasons = [15,25,35,45]
 
   # Quarter codes are YYYY-QQ, where QQ is 15 (Fall), 25 (Winter), 35 (Spring)
   # and 45 (Summer)
   def validate_each(record, attribute, value)
     quarter = value
-    if quarter.presence
-      year = quarter / 100
-      season = (quarter - (year * 100))
-      if bad_year?(year) || bad_season?(season) || bad_length?(quarter)
-        record.errors[attribute] << (options[:message] || "is not a valid quarter code")
-        return false
-      else
-        return true
-      end
-    else
+    case
+    when quarter.nil? || quarter.blank?
       record.errors[attribute] << (options[:message] || "cannot be null")
       return false
+    when bad_length?(quarter)
+      record.errors[attribute] << (options[:message] || "is not 6 digits long")
+      return false
+    when bad_year?(quarter)
+      record.errors[attribute] << (options[:message] || "is not a valid year")
+      return false
+    when bad_season?(quarter)
+      record.errors[attribute] << (options[:message] || "is not a valid season")
+      return false
+    else
+      return true
     end
   end
 
@@ -25,12 +29,14 @@ class QuarterValidator < ActiveModel::EachValidator
       quarter.to_s.length != 6
     end
 
-    def bad_year?(year)
+    def bad_year?(quarter)
+      year = quarter / 100
       (year > (Time.now.year + 10)) || year < 1980
     end
 
-    def bad_season?(season)
-      not ([15,25,35,45].include? season)
+    def bad_season?(quarter)
+      season = quarter % 100
+      not @@valid_seasons.include?(season)
     end
 
 end
