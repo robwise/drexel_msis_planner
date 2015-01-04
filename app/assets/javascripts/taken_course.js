d6bd48515dad181585421c3614be120a17431d6c
Rob Wise <robert.wise@outlook.com>
2015-01-03 22:45:38 -0500

    fix minor formatting issues

---
+++ app/assets/javascripts/taken_course.js
@@ -0,0 +1,14 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// Remove the modal content when it is hidden
+// var ready;
+// ready = function() {
+//     $('#took-this-modal').on('hidden.bs.modal', function (e) {
+//         $('#took-this-modal').remove();
+//     });
+// };
+
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
