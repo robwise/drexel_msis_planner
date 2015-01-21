60c8a42245eab043d0bc1d996dfe530bfc4da40a
Rob Wise <robert.wise@outlook.com>
2015-01-21 04:19:18 -0500

    fix HTML 5 modal autofocus

--- app/assets/javascripts/taken_course.js
+++ app/assets/javascripts/taken_course.js
@@ -1,13 +1,14 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
 
 // Remove the modal content when it is hidden
 var ready;
 ready = function() {
-    // alert("ready called");
     $('main').on('hidden.bs.modal', '.modal', function (e) {
         $('.modal').remove();
+    });
+    $('main').on('shown.bs.modal', '.modal', function (e) {
+        $('#taken_course_quarter').focus();
     });
 };
 
--- spec/features/taken_courses/taken_course_edit_spec.rb
+++ spec/features/taken_courses/taken_course_edit_spec.rb
@@ -8,35 +8,52 @@ feature "Editing an existing taken course" do
   scenario "the 'took this' link does not appear for existing course" do
     signin_user user
     visit courses_path
+
     expect(page).not_to have_content("took this")
   end
+
   scenario "the edit for the course link appears on the user dashboard" do
     signin_user user
     visit root_path
+
     expect(page).to have_link("edit")
   end
+
   scenario "the modal appears", :js, speed: "slow" do
     js_signin_user user
     visit root_path
+
     click_on "edit"
     expect(page).to have_text("Edit when you took #{course.full_id}:
       #{course.title.titleize}")
   end
+
   scenario "updates successfully", :js, speed: "slow" do
     js_signin_user user
+    visit root_path
+
     click_on "edit"
     fill_in "Quarter", with: 201425
     click_on "Update"
     expect(page).to have_text("Course history updated")
   end
+
   scenario "closing a modal and reopening another", :js, speed: "slow" do
-    taken_course2 = create :taken_course, user: user
+    course2 = create :course
+    taken_course2 = create :taken_course, course: course2, user: user
     js_signin_user user
-    expect(page).to have_text taken_course2.course.full_id
-    within("##{taken_course2.course.short_id}") { click_on "edit" }
-    click_on "Close"
-    within("##{course.short_id}") { click_on "edit" }
+    visit root_path
+
+    expect(page).to have_text(course.full_id)
+    expect(page).to have_text(course2.full_id)
+    find_by_id("edit-#{course.short_id}").click
     expect(page).to have_text("Edit when you took #{course.full_id}:
       #{course.title.titleize}")
+    click_on "Cancel"
+    # find_by_id("modal-close-button2").click
+    find_by_id("edit-#{course2.short_id}").click
+    # find_by_id("edit-#{course2.short_id}").click
+    expect(page).to have_text("Edit when you took #{course2.full_id}:
+      #{course2.title.titleize}")
   end
 end
