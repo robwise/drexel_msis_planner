fbce8464e73effd11e45f2f422f108ddf10e7b39
Rob Wise <robert.wise@outlook.com>
2015-02-18 04:10:17 -0500

    change course-index-box to extend clearfix instead of adding it as a class in the html

--- spec/features/courses/course_index_spec.rb
+++ spec/features/courses/course_index_spec.rb
@@ -1,14 +1,11 @@
 feature "Visiting course index page" do
-  before { visit courses_path }
-
-  scenario "user sees the proper title" do
-    expect(page).to have_title(full_title("Courses"))
-    expect(page).to have_content("Courses")
-  end
-
-  scenario "user sees the courses" do
+  scenario "visitor sees courses without logged-in only content" do
     course = create :course
     visit courses_path
+    expect(page).to have_title(full_title("Courses"))
+    expect(page).to have_content("Courses")
     expect(page).to have_content(course.title)
+    expect(page).not_to have_content("taken")
+    expect(page).not_to have_content("took this")
   end
 end
