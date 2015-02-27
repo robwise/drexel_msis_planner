e6c8017828de95549d319b0b63fbbc1890e02f37
Rob Wise <robert.wise@outlook.com>
2015-02-27 02:40:53 -0500

    add course edit spec tests for visitor and non-admi

--- spec/features/courses/course_edit_spec.rb
+++ spec/features/courses/course_edit_spec.rb
@@ -1,26 +1,45 @@
 feature "Editing a course" do
   let!(:course) { create(:course) }
-  let!(:admin) { create(:user, :admin) }
-  before { signin_user admin }
+  feature "as an admin" do
+    let!(:admin) { create(:user, :admin) }
+    before { signin_user admin }
 
-  scenario "using valid attributes" do
-    visit courses_path
-    expect(page).to have_link("edit")
-    click_on "edit"
-    expect(page).to have_title(full_title("Edit Course"))
-    expect(page).to have_content(course.full_id)
-    course.department = "ECON"
-    fill_course_form(course)
-    course_in_database = Course.find_by(id: course.id)
-    expect(course_in_database.department).to eq("ECON")
+    scenario "using valid attributes" do
+      visit courses_path
+      expect(page).to have_link("edit")
+      click_on "edit"
+      expect(page).to have_title(full_title("Edit Course"))
+      expect(page).to have_content(course.full_id)
+      course.department = "ECON"
+      fill_course_form(course)
+      course_in_database = Course.find_by(id: course.id)
+      expect(course_in_database.department).to eq("ECON")
+    end
+
+    scenario "using invalid attributes" do
+      visit edit_course_path(course)
+      course.level = -99
+      fill_course_form(course)
+      course_in_database = Course.find_by(id: course.id)
+      expect(course_in_database.level).not_to eq(-99)
+      expect(page).to have_selector(".alert", "Error updating course.")
+    end
   end
 
-  scenario "using invalid attributes" do
-    visit edit_course_path(course)
-    course.level = -99
-    fill_course_form(course)
-    course_in_database = Course.find_by(id: course.id)
-    expect(course_in_database.level).not_to eq(-99)
-    expect(page).to have_selector(".alert", "Error updating course.")
+  feature "as a non-admin" do
+    let!(:user) { create(:user) }
+    before { signin_user user }
+
+    scenario "is not possible" do
+      visit courses_path
+      expect(page).not_to have_link("edit")
+    end
+  end
+
+  feature "as a visitor" do
+    scenario "is not possible" do
+      visit courses_path
+      expect(page).not_to have_link("edit")
+    end
   end
 end
