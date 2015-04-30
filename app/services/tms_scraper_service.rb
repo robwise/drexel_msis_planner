53c1b5834e29b3279fa743edff5770e1a02b766f
Rob Wise <robert.wise@outlook.com>
2015-04-29 21:35:49 -0400

    reformat scraper service and tests

--- app/services/tms_scraper_service.rb
+++ app/services/tms_scraper_service.rb
@@ -4,15 +4,22 @@ class TMSScraperService
 
   SCRAPER_URL = "https://nameless-tor-6040.herokuapp.com/course/"
 
+  def update_all_courses_with_scraped_data
+    update_courses_with_scraped_data(Course.pluck(:level))
+  end
+
   def update_courses_with_scraped_data(course_levels)
-    course_levels.each { |course_level| update_course(course_level) }
+    course_levels.each do |course_level|
+      update_course_with_scraped_data(course_level)
+    end
   end
 
   def update_course_with_scraped_data(course_level)
     course_json = request_data_for_course(course_level)
     parsed_data = parse_course_json(course_json)
+    return false if parsed_data.nil?
     course = Course.find_by(level: course_level)
-    course.update!(title: parsed_data[:title], description: parsed_data[:description])
+    course.update!(parsed_data[:course_data])
     # course.prerequisite.update!(raw_text: parsed_data[:prerequisite])
     # course.corequisite.update!(raw_text: parsed_data[:corequisite])
   end
@@ -24,15 +31,16 @@ class TMSScraperService
   end
 
   def parse_course_json(response)
-    # return nil unless response.code == 200
+    return nil unless response.code == "200"
+
     course_json = JSON.parse response.body
-    parsed_data = {}
-    course_data = {}
-    parsed_data[:description] = course_json["courseDescription"]
-    parsed_data[:title] = course_json["title"]
-    parsed_data[:course_data] = course_data
-    parsed_data[:corequisite] = course_json["corequisite"]
-    parsed_data[:prerequisite] = course_json["prerequisite"]
-    parsed_data
+    {
+      course_data: {
+        description: course_json["courseDescription"],
+        title: course_json["title"]
+      },
+      corequisite: course_json["corequisite"],
+      prerequisite: course_json["prerequisite"]
+    }
   end
 end
--- spec/services/tms_scraper_service_spec.rb
+++ spec/services/tms_scraper_service_spec.rb
@@ -1,15 +1,25 @@
 describe TMSScraperService do
-
-  let!(:course) do
+  let(:course) do
     create :course, level: "530", description: "a silly description"
+  end
+  let(:other_course) do
+    create :course, level: "605", description: "other silly description"
   end
 
   describe "#update_course_with_scraped_data" do
     it "updates the course description" do
+      expect { subject.update_all_courses_with_scraped_data }
+        .to change { course.reload.description }
+    end
+  end
+
+  describe "#update_all_courses_with_scraped_data" do
+    it "updates the course descriptions" do
       old_description = course.description
-      expect(course.level).not_to be_nil
-      subject.update_course_with_scraped_data(course.level)
+      other_old_description = other_course.description
+      subject.update_all_courses_with_scraped_data
       expect(course.reload.description).not_to eq old_description
+      expect(other_course.reload.description).not_to eq other_old_description
     end
   end
 end
