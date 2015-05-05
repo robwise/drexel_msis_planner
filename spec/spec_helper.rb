c5644357d9a14f4ee01f380decb9616bafea6051
Rob Wise <robert.wise@outlook.com>
2015-05-04 23:17:53 -0400

    attempt to silence long backtraces in rspec and don't use doc format unless single file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,12 +31,12 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  # if config.files_to_run.one?
+  if config.files_to_run.one?
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    # config.default_formatter = 'doc'
-  # end
+    config.default_formatter = 'doc'
+  end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
@@ -78,4 +78,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  # Shorten backtrace
+  config.full_backtrace = false
 end
