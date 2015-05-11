417fe40572284b4dbad14ca9a5aca961f7a92ded
Rob Wise <robert.wise@outlook.com>
2015-05-11 03:42:31 -0400

    remove obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
     end
   end
   match "users/:id" => "users#destroy", via: :delete, as: :destroy_user
-  # delete "destroy_user_session", controller: "devise/sessions", action: :destroy
 end
