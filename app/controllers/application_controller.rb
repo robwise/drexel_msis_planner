3563c9a54c49932ea957d3e0105d37d199bef620
Rob Wise <robert.wise@outlook.com>
2015-05-13 02:20:34 -0400

    remove setting the active plan in ApplicationController
    This setting was super hacky and caused a lot of unnecessary hits on the database. Instead, create a new PagesController action for loading the active plan.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,
                 if: :devise_controller?
 
-  # Loads the user's active plan since it's required on all pages
-  before_action :load_users_active_plan
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -18,9 +15,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
-  end
-
-  def load_users_active_plan
-    @active_plan = current_user.nil? ? nil : current_user.active_plan
   end
 end
