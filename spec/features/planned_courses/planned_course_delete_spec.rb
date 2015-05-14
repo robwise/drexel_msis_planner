60356b41791c966f825d9b824b20f88ebbd29c10
Rob Wise <robert.wise@outlook.com>
2015-05-14 19:38:26 -0400

    remove expecation of a confirmation message when deleting a planned course

--- spec/features/planned_courses/planned_course_delete_spec.rb
+++ spec/features/planned_courses/planned_course_delete_spec.rb
@@ -8,12 +8,5 @@ feature "Deleting a planned course", js: true, slow: true do
     visit root_path
     click_button "unplan"
     expect(page).not_to have_content(planned_course.full_id)
-    expect(page).to have_deletion_message
-  end
-
-  private
-
-  def have_deletion_message
-    have_content "#{course.full_id} removed from plan"
   end
 end
