2a37462cd66251b7b9788f160eec02eac2762b26
Rob Wise <robert.wise@outlook.com>
2015-05-14 20:24:46 -0400

    fix PlannedCourseDeleteSpec Planner test to actually use planner

--- spec/features/planned_courses/planned_course_delete_spec.rb
+++ spec/features/planned_courses/planned_course_delete_spec.rb
@@ -1,11 +1,12 @@
-feature "Deleting a planned course", js: true, slow: true do
+feature "Deleting a planned course" do
   let!(:user) { create :user }
-  let!(:plan) { create :plan }
+  let!(:plan) { create :plan, user: user }
   let!(:planned_course) { create :planned_course, plan: plan }
-  before { js_signin_user user }
+  before { signin_user user }
 
   scenario "signed in user deletes planned course from planner page" do
-    visit root_path
+    visit planner_path
+    expect(page).to have_button("unplan")
     click_button "unplan"
     expect(page).not_to have_content(planned_course.full_id)
   end
